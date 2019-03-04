8926e7360b78844eb02f680d06ad4cdc3f1365e1
NicholasNeto <nicholasneto@outlook.com>
2019-03-04 14:45:54 -0300

    render two component in the same component

---
+++ react-exercicios/ex-fifth/FamilyNicols.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Member from './member'
+
+
+export default () => (
+    <div>
+        <Member name='Thaly' lastname='Neto' />
+        <Member name='Katia' lastname='Neto' />
+        <Member name='Isa' lastname='Neto' />
+        
+    </div> 
+
+)
---
+++ react-exercicios/ex-fifth/index.jsx
@@ -0,0 +1,5 @@
+import  React from 'react'
+import ReactDOM from 'react-dom'
+import Familia from './FamilyNicols'
+
+ReactDOM.render( <Familia /> , document.getElementById('app'))
