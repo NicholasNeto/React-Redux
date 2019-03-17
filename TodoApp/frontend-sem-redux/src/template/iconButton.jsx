f1cac397ee46a75bd9b86cae12466280d2581a08
NicholasNeto <nicholasneto@outlook.com>
2019-03-17 15:38:04 -0300

    create component grid & iconButton and use in TodoForm

---
+++ TodoApp/frontend-sem-redux/src/template/iconButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const IconButton = (props) => {
+
+    if(props.hide){
+        return null 
+    } else{
+        return (
+                <button className={'btn btn-' + props.style}>
+                    <i className={'fa fa-' + props.icon}></i>
+                </button>
+        )
+    }
+
+}
+
+
+export default IconButton
+
+
