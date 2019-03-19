1e65dcc0c69e0a2ae4bbcda93722c9edf2475588
NicholasNeto <nicholasneto@outlook.com>
2019-03-18 21:57:59 -0300

    implements task in list

--- TodoApp/frontend-sem-redux/src/todo/todoForm.jsx
+++ TodoApp/frontend-sem-redux/src/todo/todoForm.jsx
@@ -5,7 +5,11 @@ import IconButton from '../template/iconButton'
 const TodoForm = (props) => (
     <div>
         <Grid cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
+            <input id='description' 
+                className='form-control'
+                placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
 
         <Grid cols='12 3 2'>
