92b66b734a00fc500ab2d91bfbd2470e42c24911
NicholasNeto <nicholasneto@outlook.com>
2019-08-06 22:14:46 -0300

    create action TODO_SEARCHED and used Middleware

--- TodoApp/frontend-com-redux/src/index.jsx
+++ TodoApp/frontend-com-redux/src/index.jsx
@@ -3,13 +3,16 @@ import ReactDom from 'react-dom'
 import App from './main/app'
 
 
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
 import {Provider} from 'react-redux'
 
 import reducers from "../src/main/reducers"
+import  promise from 'redux-promise';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(reducers)
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDom.render(
     <Provider store={store}>
